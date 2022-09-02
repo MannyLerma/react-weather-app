aed110de2279b4324f8b53325993acab04f99ffc
Emmanuel Lerma <Emmanuellerma96@gmail.com>
2022-09-02 14:10:04 -0500

    Build the current weather component

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f7b276ab94msh967a2709599d556p168c71jsnc90c7fba2906',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
